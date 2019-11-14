2a309228f7ff465efe9771b65b1039bee5c4c145
IsmaelJdz <ismael_br7@hotmail.com>
2019-11-14 17:47:16 -0600

    Icono y componente publicaciones, validando perfil

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import Menu from "./Menu";
 import Usuarios from "./Usuarios";
+import Publicaciones from "./Publicaciones";
 
 const Tareas = () => <div>Tareas</div>;
 
 const App = () => (
   <BrowserRouter>
-    <Menu />  
+    <Menu />
     <div className="margen">
       <Route exact path="/" component={Usuarios} />
       <Route exact path="/tareas" component={Tareas} />
+      <Route exact path="/publicaciones/:key" component={Publicaciones} />
     </div>
   </BrowserRouter>
 );
